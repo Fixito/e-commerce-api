cacd3f9d406042c19235520f57c4b9c75aadeb93
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-07-23 13:00:48 +0200

    Added few fixes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const express = require('express');
 const app = express();
 
 // rest of the packages
-const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 //security packages
@@ -43,24 +42,11 @@ app.use(cors());
 app.use(xss());
 app.use(mongosanitize());
 
-app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use(express.static('./public'));
 app.use(fileUpload());
-
-app.get('/', (req, res) => {
-  console.log(req.cookies);
-  res.send('E-commerce api');
-});
-
-app.get('/api/v1', (req, res) => {
-  // console.log(req.cookies);
-  // cookie signé
-  console.log(req.signedCookies);
-  res.send('E-commerce api');
-});
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
